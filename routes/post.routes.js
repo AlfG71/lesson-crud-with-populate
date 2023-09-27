a45e5ff8eb36444f3f4a68119c28e12c2ae0f5fc
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-09-27 09:49:22 -0700

    finished populating

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -22,7 +22,50 @@ router.get("/post-create", (req, res) => {
 
 // <form action="/post-create" method="POST">
 
-// ... your code here
+router.post('/post-create', (req, res, next) => {
+  const { title, content, author } = req.body;
+  // 'author' represents the ID of the user document
+
+  Post.create({ title, content, author })
+    .then(dbPost => {
+      // when the new post is created, the user needs to be found and its posts updated with the
+      // ID of newly created post
+      return User.findByIdAndUpdate(author, { $push: { posts: dbPost._id } });
+    })
+    .then(() => res.redirect('/posts')) // if everything is fine, redirect to list of posts
+    .catch(err => {
+      console.log(`Err while creating the post in the DB: ${err}`);
+      next(err);
+    });
+});
+
+router.get('/', (req, res, next) => {
+  Post.find()
+    .populate('author') // --> we are saying: give me whole user object with this ID (author represents an ID in our case)
+    .then(posts => {
+      console.log("Posts from the DB: ", posts);
+      res.render('posts/list', { posts });
+    })
+    .catch(err => {
+      console.log(`Err while getting the posts from the DB: ${err}`);
+      next(err);
+    });
+});
+
+router.get('/details/:postId', (req, res, next) => {
+  const { postId } = req.params;
+
+  Post.findById(postId)
+    .populate('author')
+    .populate({path: "comments",
+      populate: {path: "author"}
+    })
+    .then(foundPost => res.render('posts/details.hbs', foundPost))
+    .catch(err => {
+      console.log(`Err while getting a single post from the  DB: ${err}`);
+      next(err);
+    });
+});
 
 // ****************************************************************************************
 // GET route to display all the posts
